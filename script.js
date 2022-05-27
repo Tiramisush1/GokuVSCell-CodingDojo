3ea8d818b4d75a42cc226caefb298390e75ff993
Tiramisush1 <martin.sandova.escarate@alumnos.sip.cl>
2022-05-26 22:10:31 -0400

    up1222

--- script.js
+++ script.js
@@ -8,26 +8,27 @@ document.getElementById("player").style.top = topValue+"px";
 
 document.onkeydown = function(e){
 console.log(e);
-if(e.keyCode == 37 && leftValue > 20) {  //left
+if(e.keyCode == 37 && leftValue > 20) {  //izquierda
     leftValue = leftValue - 10;
     document.getElementById("player").style.left = leftValue + "px";
     document.getElementById("player").style.backgroundImage = "url('imag/gokuleft.png')"
 }
-else if (e.keyCode == 39 && leftValue < 830) { //right
+else if (e.keyCode == 39 && leftValue < 785) { //derecha
     leftValue = leftValue + 10;         
     document.getElementById("player").style.left = leftValue + "px";
     document.getElementById("player").style.backgroundImage = "url('imag/gokuright.png')"
 }
-else if (e.keyCode == 40 && topValue < 500) {  //down
+else if (e.keyCode == 40 && topValue < 500) {  //abajo
     topValue = topValue + 10;
     document.getElementById("player").style.top = leftValue + "px";
     document.getElementById("player").style.backgroundImage = "url('imag/gokudown.png')"
 }
-else if (e.keyCode == 38 && topValue > 60) { //up
+else if (e.keyCode == 38 && topValue > 60) { //arriba
     topValue = topValue - 10;
     document.getElementById("player").style.top = leftValue + "px";
     document.getElementById("player").style.backgroundImage = "url('imag/gokuup.png')"
 }
+
 
 update();
 }
