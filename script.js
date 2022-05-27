accf15dc819d71792be608fcb6bbc6f07e88847a
Tiramisush1 <martin.sandova.escarate@alumnos.sip.cl>
2022-05-26 22:12:48 -0400

    111123

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ document.getElementById("player").style.left = leftValue+"px";
 document.getElementById("player").style.top = topValue+"px";
 }
 
+
+
 document.onkeydown = function(e){
 console.log(e);
 if(e.keyCode == 37 && leftValue > 20) {  //izquierda
