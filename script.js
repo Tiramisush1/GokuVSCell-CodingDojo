4b8ca3ed97585f4dacb4936c2c26a7438f0e2661
Tiramisush1 <martin.sandova.escarate@alumnos.sip.cl>
2022-05-26 21:38:05 -0400

    up1

--- script.js
+++ script.js
@@ -10,15 +10,23 @@ document.onkeydown = function(e){
 console.log(e);
 if(e.keyCode == 37 && leftValue > 20) {  //left
     leftValue = leftValue - 10;
+    document.getElementById("player").style.left = leftValue + "px";
+    document.getElementById("player").style.backgroundImage = "url('imag/gokuleft.png')"
 }
 else if (e.keyCode == 39 && leftValue < 830) { //right
     leftValue = leftValue + 10;         
+    document.getElementById("player").style.left = leftValue + "px";
+    document.getElementById("player").style.backgroundImage = "url('imag/gokuright.png')"
 }
 else if (e.keyCode == 40 && topValue < 500) {  //down
     topValue = topValue + 10;
+    document.getElementById("player").style.top = leftValue + "px";
+    document.getElementById("player").style.backgroundImage = "url('imag/gokudown.png')"
 }
 else if (e.keyCode == 38 && topValue > 60) { //up
     topValue = topValue - 10;
+    document.getElementById("player").style.top = leftValue + "px";
+    document.getElementById("player").style.backgroundImage = "url('imag/gokuup.png')"
 }
 
 update();
